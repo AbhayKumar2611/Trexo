b1527eb7a00e9db49b3edf8ec413aa5a6da736a0
Abhay Kumar <kumarabhay26111996@gmail.com>
2025-05-02 12:28:41 +0530

    Some Files Modified

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,17 +1,3 @@
-// import React from "react";
-// import UserCardsSection from "./UserCard";
-// import "./UserList.css";
-
-// const UserList = ({ users }) => {
-//   return (
-//     <div className="user-list-container">
-//       <UserCardsSection users={users} />
-//     </div>
-//   );
-// };
-
-// export default UserList;
-
 import React from "react";
 import UserCardsSection from "./UserCard";
 import "./UserList.css";
